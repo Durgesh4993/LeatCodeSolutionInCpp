d1cb82a1590ad79c37a4598657452aba3c830382
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-04-14 16:50:12 +0530

    Update and rename Number of 1 Bits to Number of 1 Bits.cpp

--- Number of 1 Bits.cpp
+++ Number of 1 Bits.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count=0;
+        
         while (n) {
             // let's and(&) each bit with 1, if the result is 1 then we increment count by 1
             if (n&1 > 0) count++;
