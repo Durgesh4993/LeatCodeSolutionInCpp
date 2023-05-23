821e9a982538d3c83575985bc8a7ee5dc8ed7f4e
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-05-23 17:59:50 +0530

    Create Kth Largest Element in a Stream.cpp

---
+++ Kth Largest Element in a Stream.cpp
@@ -0,0 +1,23 @@
+class KthLargest {
+private:
+    int k;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        return minHeap.top();
+    }
+};
