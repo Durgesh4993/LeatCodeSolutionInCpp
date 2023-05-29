d706de1011888e3fefe55bfdd91409d11d0d1980
Durgesh kumar prajapati <98798977+Durgesh4993@users.noreply.github.com>
2023-05-29 18:15:30 +0530

    Create Design Parking System.cpp

---
+++ Design Parking System.cpp
@@ -0,0 +1,25 @@
+class ParkingSystem {
+public:
+        int big;
+        int medium;
+        int small;
+
+    ParkingSystem(int big, int medium, int small) {
+         this->big = big;
+         this->medium = medium;
+         this->small = small;
+    }
+    
+    bool addCar(int carType) {
+        
+         if(carType == 1 && big == 0 || carType == 2 && medium == 0 || carType == 3 && small == 0)
+          return false;
+
+          if(carType == 1) big--;
+          if(carType == 2) medium--;
+          if(carType == 3) small--;
+
+        return true;
+    }
+};
+
